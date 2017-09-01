2d10d81610c8d3ebdc1ef0a350ba647c7609e4f0
chenyajun <1451417401@qq.com>
2017-09-01 11:45:30 +0800

    karma.conf

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -67,17 +67,18 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS'],
+    //browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
